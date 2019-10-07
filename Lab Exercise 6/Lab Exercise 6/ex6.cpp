a78daa163200036885f76d334bf32429bc3300cb
Michaelciv1 <56279813+Michaelciv1@users.noreply.github.com>
2019-10-07 15:56:38 -0700

    Add files via upload

--- Lab Exercise 6/Lab Exercise 6/ex6.cpp
+++ Lab Exercise 6/Lab Exercise 6/ex6.cpp
@@ -13,13 +13,13 @@ int main() {
     ofstream myfile;
     myfile.open ("ex6output.txt");
     srand(static_cast<unsigned int>(time(0)));
-    int anodanumber = 0;
+    char character = 0;
     
     for (int number =0; number < 25; number++)
     {
-        anodanumber = (rand() % 50) + 50;
-        myfile << anodanumber << "\n";
-        cout << anodanumber << endl; //This is just for me to see the output
+        character = (rand() % 50) + 50;
+        myfile << character << "\n";
+        cout << character << endl; //This is just for me to see the output
     }
     myfile.close();
 }
