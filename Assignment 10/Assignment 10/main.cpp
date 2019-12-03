81ca126ba2c5b646e56bd416a727831349de3966
Michaelciv1 <56279813+Michaelciv1@users.noreply.github.com>
2019-12-02 23:35:43 -0800

    Add files via upload

--- Assignment 10/Assignment 10/main.cpp
+++ Assignment 10/Assignment 10/main.cpp
@@ -13,11 +13,14 @@ using namespace std;
 
 const int MAX_SIZE = 1000;
 const int NUM_FIELDS_PER_LINE = 7;
+const int NUM_MARKET_DAYS = 8;
 
-int upDays();
-void downDays();
-void maxClose();
-void avgClose();
+int upDays(int marketDays, float open[], float close[]);
+int downDays(int marketDays, float open[], float close[]);
+float gain(int marketDays, float open[], float currentClose);
+float prctGain(int marketDays, float open[], float currentClose);
+float maxClose(int marketDay, float close[]);
+float avgClose(int marketDay, float close[]);
 
 
 int main() {
@@ -35,6 +38,7 @@ int main() {
     float open[MAX_SIZE];
     float close[MAX_SIZE];
     string field[NUM_FIELDS_PER_LINE];
+    int marketDays[NUM_MARKET_DAYS] = {5,10,20,50,100,200,500,1000};
     
     int lineCnt = 0;
     string line;
@@ -60,13 +64,66 @@ int main() {
         }
     
     }
+    float currentClose = close[0];
     output << "Stock: " << stockName << endl;;
     output << "Current Date: " << date[0] << endl;
     output << "Current Close: " << close[0] << endl;
     output << "Market Days" << endl;
     output << "-----------" << endl;
+    
+    for (int idx = 0; idx < NUM_MARKET_DAYS; idx++){
+        int marketDay = marketDays[idx];
+        output << marketDay << " " << date[marketDay - 1] << " " << open[marketDay - 1] << " " << upDays(marketDay, open, close) << " " << downDays(marketDay, open, close) << " " << gain(marketDay, open, currentClose) << " " << prctGain(marketDay, open, currentClose) << " " << maxClose(marketDay, close) << " " << avgClose(marketDay, close) << endl;
+    }
 }
 
-int upDays() {
-    return 0;
+int upDays(int marketDays, float open[], float close[]) {
+    int up = 0;
+    for (int idx = 0; idx < marketDays; idx++)
+    {
+        if (open[idx] < close[idx]){
+            up++;
+        }
+    }
+    return up;
+}
+
+int downDays(int marketDays, float open[], float close[]) {
+    int down = 0;
+    for (int idx = 0; idx < marketDays; idx++)
+    {
+        if (open[idx] > close[idx]){
+            down++;
+        }
+    }
+    return down;
+}
+
+float gain(int marketDay,float open[], float currentClose){
+    float gain = currentClose - open[marketDay-1];
+    return gain;
+}
+
+float prctGain(int marketDay,float open[], float currentClose){
+    float gain = currentClose - open[marketDay-1];
+    float pGain = gain/open[marketDay-1] * 100;
+    return pGain;
+}
+
+float maxClose(int marketDay, float close[]){
+    float currentMax = close[0];
+    for (int idx = 0; idx < marketDay; idx++){
+        if (close[idx] > currentMax){
+            currentMax = close[idx];
+        }
+    }
+    return currentMax;
+}
+
+float avgClose(int marketDay, float close[]){
+    float total = 0;
+    for (int idx = 0; idx < marketDay; idx++){
+        total += close[idx];
+        }
+    return total/marketDay;
 }
