26152f9393fcad507cb8304f2f7892578b487b11
Michaelciv1 <56279813+Michaelciv1@users.noreply.github.com>
2019-12-11 06:27:18 -0800

    Add files via upload

---
+++ Assignment 11/ass11.cpp
@@ -0,0 +1,90 @@
+//  main.cpp
+//  Assignment 11
+//  Created by Michael Wallerius on 12/4/19.
+//  Code::Blocks on Windows 10
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void fillArray(ifstream &file, string array[], int size);
+void searchArray(ofstream &output, string dictionary[], string keyword[], int DNUMBER, int KNUMBER); //used actual array names to avoid confusing myself for this specific method
+void swapValues(string array[], int idx1, int idx2);
+int smallestValueIndex(const string array[], int length, int startIndex = 0);
+void selectionSort(string array[], int length);
+
+const int DNUMBER = 20000;
+const int KNUMBER = 84;
+
+int main() {
+    ifstream dInput("unsorted_dictionary.txt");
+    ifstream kInput("keywords.txt");
+    ofstream output("output.txt");
+
+    if (!dInput.is_open() || !kInput.is_open() || !output.is_open()){
+        exit(EXIT_FAILURE);
+    }
+
+    string dictionary[DNUMBER];
+    string keyword[KNUMBER];
+
+    fillArray(dInput, dictionary, DNUMBER);
+    fillArray(kInput, keyword, KNUMBER);
+
+    selectionSort(dictionary, DNUMBER);
+    selectionSort(keyword, KNUMBER);
+
+    searchArray(output, dictionary, keyword, DNUMBER, KNUMBER);
+
+}
+
+void fillArray(ifstream &file, string array[], int size){
+    for (int idx = 0; idx < size; idx++){
+        file >> array[idx];
+    }
+}
+
+void searchArray(ofstream &output, string dictionary[], string keyword[], int DNUMBER, int KNUMBER){
+    int numFound = 0;
+    for (int x = 0; x < KNUMBER; x++){
+        int foundState = 0;
+        for (int b = 0; b < DNUMBER; b++){
+            if (keyword[x] == dictionary[b]){
+                numFound++;
+                foundState = 1;
+            }
+        }
+        if (foundState == 0){
+            output << "keyword not found: " << keyword[x] << endl;
+        }
+    }
+    cout << "Number of keywords not found = " << KNUMBER - numFound << endl;
+}
+
+void swapValues(string array[], int idx1, int idx2){
+    string temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
+}
+
+int smallestValueIndex(const string array[], int length, int startIndex){
+    int minIdx = startIndex;
+
+    for (int idx = startIndex+1; idx < length; idx++){
+        if (array[idx] < array[minIdx])
+            minIdx = idx;
+    }
+
+    return minIdx;
+}
+
+void selectionSort(string array[], int length){
+    int minIdx;
+
+    for (int idx = 0; idx < length; idx++){
+        minIdx = smallestValueIndex(array, length, idx);
+        if (minIdx != idx);
+            swapValues(array, minIdx, idx);
+    }
+}
